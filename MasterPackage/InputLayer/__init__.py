1e6e613d5b584e884dee5184a70e0c00d8b715b1
Frank Hu <45833243+FranklinHu1@users.noreply.github.com>
2021-06-29 20:02:24 -0400

    Minor switch over from input_layer_pairwise_linear_full

--- MasterPackage/InputLayer/__init__.py
+++ MasterPackage/InputLayer/__init__.py
@@ -12,7 +12,7 @@ from .input_layer_DFTB import Input_layer_DFTB
 from .input_layer_DFTB_val import Input_layer_DFTB_val
 from .input_layer_hubbard import Input_layer_hubbard
 from .input_layer_pairwise_linear_joined import Input_layer_pairwise_linear_joined
-from .input_layer_pairwise_linear_full import Input_layer_pairwise_linear
+from .input_layer_pairwise_linear import Input_layer_pairwise_linear
 from .input_layer_value import Input_layer_value
 from .reference_energy import Reference_energy
 from .repulsive_new import repulsive_energy
