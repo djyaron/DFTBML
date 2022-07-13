255afd46477a81f4fc1451ead96f5146936121fe
Frank Hu <45833243+FranklinHu1@users.noreply.github.com>
2022-07-12 22:27:29 -0400

    Fixed repulsive solo training module

--- MasterPackage/Training/__init__.py
+++ MasterPackage/Training/__init__.py
@@ -9,4 +9,5 @@ from .training_loop import training_loop
 from .util import write_output_skf, write_output_lossinfo, exclude_R_backprop,\
     check_split_mapping_disjoint, sort_gammas_ctracks, charge_update_subroutine
 from .repulsive_training_only import train_repulsive_model, flatten_batches,\
-    reconstruct_batches, write_out_repulsive_model
+    reconstruct_batches, write_out_repulsive_model, auto_flatten_batches, copy_molecules_from_analysis_dir,\
+        train_all_repulsive_models
--- MasterPackage/Training/repulsive_training_only.py
+++ MasterPackage/Training/repulsive_training_only.py
@@ -26,9 +26,11 @@ from Training import sort_gammas_ctracks
 from InputParser import parse_input_dictionaries,\
     inflate_to_dict
 from functools import reduce
-from DFTBRepulsive import SKFBlockCreator
+from DFTBrepulsive import SKFBlockCreator
 from PlottingUtil import read_skf_set
-from SKF import get_spl_xydata
+from MasterConstants import atom_nums
+import re, shutil
+
 #%% Code behind
 
 def reformat_molecule_data(mol: Dict) -> None:
@@ -92,12 +94,13 @@ def train_repulsive_model(dset_dir: str, settings_filename: str, defaults_filena
     split_mapping = {0 : [[0], [1]]}
     train_gammas, train_c_trackers, valid_gammas, valid_c_trackers = sort_gammas_ctracks(split_mapping, 0, gammas, c_trackers)
     #Load in the batched data with the DFTB+ informationa added in
-    T_batch_name = os.path.join(dset_dir, 'Fold0', 'batch_added.p')
-    V_batch_name = os.path.join(dset_dir, 'Fold1', 'batch_added.p')
+    T_batch_name = os.path.join(os.getcwd(), dset_dir, 'Fold0', 'processed_batches.p')
+    V_batch_name = os.path.join(os.getcwd(), dset_dir, 'Fold1', 'processed_batches.p')
     training_batches = pickle.load(open(T_batch_name, 'rb'))
     validation_batches = pickle.load(open(V_batch_name, 'rb'))
-    assert(len(training_batches) == 100)
+    # assert(len(training_batches) == 250)
     assert(len(training_batches) != len(validation_batches))
+    # assert(len(validation_batches) == 63)
     #Correct the data
     for batch in training_batches: 
         for mol in batch:
@@ -129,6 +132,33 @@ def reconstruct_batches(flattened_batch: List[Dict], batch_size: int) -> None:
         batches.append(current_batch)
     return batches
 
+def auto_flatten_batches(dset_dir: str) -> None:
+    r"""Goes through a dataset and flattens all the batches
+    
+    Arguments:
+        dset_dir (str): The dataset directory to flatten the batches for
+    
+    Returns:
+        None
+    
+    Notes: Goes through the directory and flattens the batch_original.p files in 
+        each of the Fold{num} directories. Saves the file as batch_flat.p in that directory
+    """
+    all_files = os.listdir(dset_dir)
+    for elem in all_files:
+        #Right now there are only two folds we care about 
+        if elem  in ['Fold0', 'Fold1']:
+            num = int(elem[-1])
+            print(f"Flattening for {elem}")
+            full_direc_path = os.path.join(os.getcwd(), dset_dir, elem)
+            batch_path = os.path.join(full_direc_path, 'batch_original.p')
+            with open(batch_path, 'rb') as handle:
+                batches = pickle.load(handle)
+            flattened_batches = flatten_batches(batches)
+            with open(os.path.join(full_direc_path, f'batch_flat_{num}.p'), 'wb') as handle:
+                pickle.dump(flattened_batches, handle)
+    print(f"All batches flattened for {dset_dir}")
+
 def write_out_repulsive_model(trained_rep_mod: DFTBRepulsiveModel, ref_set: str, dest: str,
                               spl_ngrid: int = 500) -> None:
     r"""Write out the repulsive model into a series of SKF files using the reference
@@ -136,10 +166,10 @@ def write_out_repulsive_model(trained_rep_mod: DFTBRepulsiveModel, ref_set: str,
         original electronic.
     
     Arguments:
-        trained_rep_mod (DFTBRepulsiveModel): The trained repulsive model
+        trained_rep_mod (DFTBRepulsiveModel): The trained repulsive model.
         ref_set (str): The path to the reference set of SKF files to use for everything but
             the repulsive spline.
-        dest (str): The destination to save the newly written SKF files to
+        dest (str): The destination to save the newly written SKF files to.
         spl_ngrid (int): The number of grid points used in writing out the spline repulsive 
             for the SKF files. Defaults to 500.
         
@@ -151,6 +181,8 @@ def write_out_repulsive_model(trained_rep_mod: DFTBRepulsiveModel, ref_set: str,
         spline block. The ref_set used in this method should be the same as the 
         SKF set used to generate the initial electronic predictions used to train 
         the repulsive model. 
+        
+        Also need to write out the repulsive parameters
     """
     ref_skset = read_skf_set(ref_set)
     creator = SKFBlockCreator()
@@ -162,8 +194,77 @@ def write_out_repulsive_model(trained_rep_mod: DFTBRepulsiveModel, ref_set: str,
     for Zs in all_Zs:
         print(f"Currently writing SKF for {Zs}")
         curr_ref_skf = ref_skset[Zs]
-        spline_block = creator.create_spline_block(curr_ref_skf['exp_coef'], xy_data[Zs])
+        spline_block = creator.create_spline_block(curr_ref_skf['exp_coef'].to_numpy()[0], xy_data[Zs])
         #Re-assign the spline block to create a new SKF
         curr_ref_skf['spline'] = spline_block
-        name = f"{Zs[0]}-{Zs[1]}.skf"
+        name = f"{atom_nums[Zs[0]]}-{atom_nums[Zs[1]]}.skf"
         curr_ref_skf.to_file(os.path.join(dest, name))
+    #Also write out the reference energy parameters
+    ref_info = trained_rep_mod.get_ref_ener_info(False)
+    with open(os.path.join(dest, "ref_params.p"), 'wb') as handle:
+        pickle.dump(ref_info, handle)
+
+def copy_molecules_from_analysis_dir(dset_dir: str, batch_size: int = 10) -> None:
+    r"""Copies the calcultion results from analysis_dir/analysis_files
+        into the corresponding Fold directory.
+    
+    Arguments:
+        dset_dir (str): The dataset directory to perform the copy operation for
+    
+    Returns:
+        None
+    
+    Notes: Also reformats to produce the batches again.
+    """
+    all_analysis_files = os.path.join(os.getcwd(), dset_dir, 'analysis_dir', 'analysis_files')
+    pattern = r'Fold[0-9]+'
+    for file in os.listdir(all_analysis_files):
+        #Only care about the pickle files containing the calculation results
+        if file.split(".")[-1] == 'p':
+            filename_splt = file.split('.')[0].split("_")
+            #Find the corresponding fold directory
+            fold_dir = list(filter(lambda x : re.match(pattern, x), filename_splt))[0]
+            src = os.path.join(all_analysis_files, file)
+            with open(src, 'rb') as handle:
+                calc_mols = pickle.load(handle)
+            calc_batches = reconstruct_batches(calc_mols, batch_size)
+            dst = os.path.join(os.getcwd(), dset_dir, fold_dir, 'processed_batches.p')
+            with open(dst, 'wb') as handle:
+                pickle.dump(calc_batches, handle)
+            print(f"Shifted {src} to {dst}")
+
+def train_all_repulsive_models(dset_dirs: List[str], batch_size: int, settings_file: str, defaults_file: str,
+                               ref_set: str) -> None:
+    r"""Reorganizes the data and trains the repulsive model on each of the datasets. 
+        It then writes out the SKF files for each of the trained models.
+    
+    Arguments:
+        dset_dirs (List[str]): The list of dataset directories to train on
+        batch_size (int): The number of molecules per batch (used for re-organization)
+        settings_file (str): Settings json file used for training the repulsive model
+        defaults_file (str): The defaults json file for training the repulsive model
+        ref_set (str): Path to the reference set of SKF files used to perform the 
+            initial DFTB+ calculations
+    
+    Returns:
+        None
+    
+    Notes: This method performs a re-organization as well as the training calculation
+        and writing output. 
+    """
+    for dset in dset_dirs:
+        print(f"Performing training for {dset}")
+        #First step is re-organize the data through copying
+        copy_molecules_from_analysis_dir(dset, batch_size)
+        #Perform the training
+        rep_mod = train_repulsive_model(dset, settings_file, defaults_file)
+        #Write the output SKF files
+        dset_name = os.path.split(dset)[-1]
+        destination = os.path.join(os.getcwd(), dset, f"{dset_name}_SKFs")
+        if (not os.path.isdir(destination)):
+            os.mkdir(destination)
+        write_out_repulsive_model(rep_mod, ref_set, destination)
+        #Last step: copy the test set
+        src = os.path.join(os.getcwd(), dset, 'test_set.p')
+        shutil.copy(src, destination)
+        
