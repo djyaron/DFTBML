b5d80d9ca609b9a291ea35a231168a88461c2798
Sage Betko <sage.betko@gmail.com>
2022-11-09 20:45:54 -0500

    fix: parametrize paramPath in ParDict constructor

--- MasterPackage/Auorg_1_1/auorg_1_1.py
+++ MasterPackage/Auorg_1_1/auorg_1_1.py
@@ -11,53 +11,56 @@ import os
 
 from SKF import SkfInfo
 
-PARAMPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Auorg_1_1", "auorg-1-1")
+PARAMPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "auorg-1-1")
 
-def ParDict():
-    elements = ['H', 'C', 'N', 'O', 'S', 'Au']
-    parDict = {el1 + '-' + el2: SkfInfo(el1 + '-' + el2, paramPath=PARAMPATH)
-               for el1 in elements for el2 in elements}
+
+def ParDict(paramPath=PARAMPATH):
+    elements = ["H", "C", "N", "O", "S", "Au"]
+    parDict = {
+        el1 + "-" + el2: SkfInfo(el1 + "-" + el2, paramPath=paramPath)
+        for el1 in elements
+        for el2 in elements
+    }
     # Spin constants (parameter W, doublju)
     # H:
     #      -0.07174
-    parDict['H-H'].SetAtomProp('Wss', -0.07174)
+    parDict["H-H"].SetAtomProp("Wss", -0.07174)
     # C:
     #      -0.03062     -0.02505
     #      -0.02505     -0.02265
-    parDict['C-C'].SetAtomProp('Wss', -0.03062)
-    parDict['C-C'].SetAtomProp('Wsp', -0.02505)
-    parDict['C-C'].SetAtomProp('Wpp', -0.02265)
+    parDict["C-C"].SetAtomProp("Wss", -0.03062)
+    parDict["C-C"].SetAtomProp("Wsp", -0.02505)
+    parDict["C-C"].SetAtomProp("Wpp", -0.02265)
     # N:
     #      -0.03318     -0.02755
     #      -0.02755     -0.02545
-    parDict['N-N'].SetAtomProp('Wss', -0.03318)
-    parDict['N-N'].SetAtomProp('Wsp', -0.02755)
-    parDict['N-N'].SetAtomProp('Wpp', -0.02545)
+    parDict["N-N"].SetAtomProp("Wss", -0.03318)
+    parDict["N-N"].SetAtomProp("Wsp", -0.02755)
+    parDict["N-N"].SetAtomProp("Wpp", -0.02545)
     # O:
     #      -0.03524     -0.02956
     #      -0.02956     -0.02785
-    parDict['O-O'].SetAtomProp('Wss', -0.03524)
-    parDict['O-O'].SetAtomProp('Wsp', -0.02956)
-    parDict['O-O'].SetAtomProp('Wpp', -0.02785)
+    parDict["O-O"].SetAtomProp("Wss", -0.03524)
+    parDict["O-O"].SetAtomProp("Wsp", -0.02956)
+    parDict["O-O"].SetAtomProp("Wpp", -0.02785)
     # S:
     #      -0.02137     -0.01699     -0.01699
     #      -0.01699     -0.01549     -0.01549
     #      -0.01699     -0.01549     -0.01549
-    parDict['S-S'].SetAtomProp('Wss', -0.02137)
-    parDict['S-S'].SetAtomProp('Wsp', -0.01699)
-    parDict['S-S'].SetAtomProp('Wsd', -0.01699)
-    parDict['S-S'].SetAtomProp('Wpp', -0.01549)
-    parDict['S-S'].SetAtomProp('Wpd', -0.01549)
-    parDict['S-S'].SetAtomProp('Wdd', -0.01549)
+    parDict["S-S"].SetAtomProp("Wss", -0.02137)
+    parDict["S-S"].SetAtomProp("Wsp", -0.01699)
+    parDict["S-S"].SetAtomProp("Wsd", -0.01699)
+    parDict["S-S"].SetAtomProp("Wpp", -0.01549)
+    parDict["S-S"].SetAtomProp("Wpd", -0.01549)
+    parDict["S-S"].SetAtomProp("Wdd", -0.01549)
     # Au:
     #      -0.01304     -0.01304   -0.00525
     #      -0.01304     -0.01304   -0.00525
     #      -0.00525     -0.00525   -0.01082
-    parDict['Au-Au'].SetAtomProp('Wss', -0.01304)
-    parDict['Au-Au'].SetAtomProp('Wsp', -0.01304)
-    parDict['Au-Au'].SetAtomProp('Wsd', -0.00525)
-    parDict['Au-Au'].SetAtomProp('Wpp', -0.01304)
-    parDict['Au-Au'].SetAtomProp('Wpd', -0.00525)
-    parDict['Au-Au'].SetAtomProp('Wdd', -0.01082)
+    parDict["Au-Au"].SetAtomProp("Wss", -0.01304)
+    parDict["Au-Au"].SetAtomProp("Wsp", -0.01304)
+    parDict["Au-Au"].SetAtomProp("Wsd", -0.00525)
+    parDict["Au-Au"].SetAtomProp("Wpp", -0.01304)
+    parDict["Au-Au"].SetAtomProp("Wpd", -0.00525)
+    parDict["Au-Au"].SetAtomProp("Wdd", -0.01082)
     return parDict
-    
