763ffb3314d5ed485cd1c4f161095dc643d19a75
Sage Betko <sage.betko@gmail.com>
2022-11-09 20:11:34 -0500

    refactor: replace additional test data references

--- MasterPackage/Tests/precompute_test.py
+++ MasterPackage/Tests/precompute_test.py
@@ -11,39 +11,54 @@ NOTE: This test does NOT guarantee correctness, only that the code can run throu
     
 TODO: Incorporate gammas precompute into the precompute testing
 """
-#%% Imports, definittions 
+#%% Imports, definittions
 import os
 import shutil
 
-from FoldManager import (compute_graphs_from_folds, generate_folds,
-                         precompute_gammas, precompute_gammas_per_fold,
-                         save_folds)
-from InputParser import (collapse_to_master_settings, inflate_to_dict,
-                         parse_input_dictionaries)
+from FoldManager import (
+    compute_graphs_from_folds,
+    generate_folds,
+    precompute_gammas,
+    precompute_gammas_per_fold,
+    save_folds,
+)
+from InputParser import (
+    collapse_to_master_settings,
+    inflate_to_dict,
+    parse_input_dictionaries,
+)
 
 from .h5handler_test import run_safety_check
-from .helpers import test_data_dir
+from .helpers import ani1_path, test_data_dir
 
 #%% Code behind
 
+
 def run_fold_gen_test():
-    allowed_Zs = [1,6,7,8]
-    heavy_atoms = [1,2,3,4,5,6,7,8]
+    allowed_Zs = [1, 6, 7, 8]
+    heavy_atoms = [1, 2, 3, 4, 5, 6, 7, 8]
     max_config = 5
-    target = {'Etot' : 'cc',
-            'dipole' : 'wb97x_dz.dipole',
-            'charges' : 'wb97x_dz.cm5_charges'}
-    data_path = os.path.join(os.getcwd(), "test_files", "ANI-1ccx_clean_fullentry.h5")
-    exclude = ['O3', 'N2O1', 'H1N1O3', 'H2']
+    target = {"Etot": "cc", "dipole": "wb97x_dz.dipole", "charges": "wb97x_dz.cm5_charges"}
+    exclude = ["O3", "N2O1", "H1N1O3", "H2"]
     lower_limit = 5
     num_folds = 6
     num_folds_lower = 3
     local_fold_molecs = "fold_molecs_internal"
 
     print("Testing fold generation")
-    folds = generate_folds(allowed_Zs, heavy_atoms, max_config, target, data_path, exclude, 
-                            lower_limit, num_folds, num_folds_lower)
+    folds = generate_folds(
+        allowed_Zs,
+        heavy_atoms,
+        max_config,
+        target,
+        ani1_path,
+        exclude,
+        lower_limit,
+        num_folds,
+        num_folds_lower,
+    )
     save_folds(folds, local_fold_molecs)
+
 
 def run_precompute_test(clear_direc: bool = True):
     print("Testing out precompute...")
@@ -52,26 +67,26 @@ def run_precompute_test(clear_direc: bool = True):
     resulting_settings_obj = parse_input_dictionaries(settings_filename, defaults_filename)
     opts = inflate_to_dict(resulting_settings_obj)
     final_settings = collapse_to_master_settings(resulting_settings_obj)
-    #Do the graph computation
+    # Do the graph computation
     compute_graphs_from_folds(final_settings, "fold_molecs_internal", True)
-    #Now do the gammas computation for the entire dataset
+    # Now do the gammas computation for the entire dataset
     precompute_gammas(opts, "fold_molecs_internal")
-    #Now do the gammas computation for each fold
+    # Now do the gammas computation for each fold
     precompute_gammas_per_fold(opts, "fold_molecs_internal")
     print("Precompute executed successfully.")
     run_safety_check("fold_molecs_internal", [0, 1, 2, 3, 4, 5])
     print("Safety check successfully executed for precomputed data")
-    #Delete the directory at the end. 
+    # Delete the directory at the end.
     if clear_direc:
         shutil.rmtree("fold_molecs_internal")
     pass
+
 
 def run_fold_precomp_tests(clear_direc: bool):
     run_fold_gen_test()
     run_precompute_test(clear_direc)
 
+
 #%% Main block
 if __name__ == "__main__":
     run_fold_precomp_tests()
-    
-
--- MasterPackage/Tests/skf_test.py
+++ MasterPackage/Tests/skf_test.py
@@ -18,109 +18,130 @@ from DFTBPlus import run_organics
 from MasterConstants import Model, atom_masses, atom_nums
 from SKF import determine_index, write_skfs
 
-from .helpers import auorg_dir, test_data_dir, ani1_path
+from .helpers import ani1_path, auorg_dir, test_data_dir
 
 #%% Code behind
 
+
 def test_determine_index():
-    r"""Tests that the proper indices are used for the model values
-    """
-    
+    r"""Tests that the proper indices are used for the model values"""
+
     print("Testing get index...")
-    
+
     dummy_mod_specs = [
-        
-        Model(oper='H', Zs=(6, 1), orb='ss'),
-        Model(oper='H', Zs=(1, 6), orb='sp'),
-        Model(oper='H', Zs=(7, 7), orb='pp_pi'),
-        Model(oper='H', Zs=(7, 7), orb='pp_sigma'),
-        
-        Model(oper='S', Zs=(6, 1), orb='ss'),
-        Model(oper='S', Zs=(1, 6), orb='sp'),
-        Model(oper='S', Zs=(7, 7), orb='pp_pi'),
-        Model(oper='S', Zs=(7, 7), orb='pp_sigma')
-        
-        ]
-    
+        Model(oper="H", Zs=(6, 1), orb="ss"),
+        Model(oper="H", Zs=(1, 6), orb="sp"),
+        Model(oper="H", Zs=(7, 7), orb="pp_pi"),
+        Model(oper="H", Zs=(7, 7), orb="pp_sigma"),
+        Model(oper="S", Zs=(6, 1), orb="ss"),
+        Model(oper="S", Zs=(1, 6), orb="sp"),
+        Model(oper="S", Zs=(7, 7), orb="pp_pi"),
+        Model(oper="S", Zs=(7, 7), orb="pp_sigma"),
+    ]
+
     target_inds = [9, 8, 6, 5, 9, 8, 6, 5]
-    
+
     for i, mod_spec in enumerate(dummy_mod_specs):
-        assert(determine_index(mod_spec) == target_inds[i])
-    
+        assert determine_index(mod_spec) == target_inds[i]
+
     print("Testing index passed successfully")
+
 
 def test_new_SKF_framework(clear_direc: bool):
     r"""Tests the SKF functionalities written by Francis in DFTBrepulsive.
-        This is accomplished by writing out SKFs from past saved models
-        (which should not fail) and then checking the value obtained by 
-        DFTB+ on those skfs against benchmarks.
+    This is accomplished by writing out SKFs from past saved models
+    (which should not fail) and then checking the value obtained by
+    DFTB+ on those skfs against benchmarks.
     """
     print("Testing new SKF framework...")
-    tol = 1E-6
+    tol = 1e-6
     skf_8020_benchmark = 4.318332359222469
     refacted_benchmark = 4.290744984048831
-    
-    #Parameters for writing SKFs
+
+    # Parameters for writing SKFs
     model_path = os.path.join(test_data_dir, "skf_8020_100knot/Split0/saved_models.p")
-    models = pickle.load(open(model_path, 'rb'))
+    models = pickle.load(open(model_path, "rb"))
     compute_S_block = True
     ref_direct = os.path.join(auorg_dir, "auorg-1-1")
     rep_mode = "old"
     dest = os.path.join(test_data_dir, "skf_8020_100knot_deriv_delete")
     spl_ngrid = 50
-    
-    #Write the SKFs using the new framework
-    write_skfs(models, atom_nums, atom_masses, compute_S_block, ref_direct, rep_mode, dest, spl_ngrid)
-    
-    #Now run them through DFTB+ with the proper settings from run_organics
+
+    # Write the SKFs using the new framework
+    write_skfs(
+        models, atom_nums, atom_masses, compute_S_block, ref_direct, rep_mode, dest, spl_ngrid
+    )
+
+    # Now run them through DFTB+ with the proper settings from run_organics
     max_config = 1
     maxheavy = 8
-    allowed_Zs = [1,6,7,8]
-    target = 'cc'
-    
-    skf_dir = os.path.join(os.getcwd(), "test_files", "skf_8020_100knot_deriv_delete")
-    
+    allowed_Zs = [1, 6, 7, 8]
+    target = "cc"
+
+    skf_dir = os.path.join(test_data_dir, "skf_8020_100knot_deriv_delete")
+
     exec_path = "C:\\Users\\fhu14\\Desktop\\DFTB17.1Windows\\DFTB17.1Windows-CygWin\\dftb+"
     pardict = Auorg_1_1.ParDict()
     rep_setting = "old"
-    
-    err_Ha, err_kcal, _ = run_organics(ani1_path, max_config, maxheavy, allowed_Zs, target, skf_dir,
-                                    exec_path, pardict, rep_setting)
-    
+
+    err_Ha, err_kcal, _ = run_organics(
+        ani1_path,
+        max_config,
+        maxheavy,
+        allowed_Zs,
+        target,
+        skf_dir,
+        exec_path,
+        pardict,
+        rep_setting,
+    )
+
     print(f"Error in Hartrees: {err_Ha}")
     print(f"Error in kcal/mol: {err_kcal}")
-    
-    assert(abs(err_kcal - skf_8020_benchmark) < tol)
-    
-    #Do another SKF set
+
+    assert abs(err_kcal - skf_8020_benchmark) < tol
+
+    # Do another SKF set
     model_path = os.path.join(test_data_dir, "refacted_joined_spline_run/Split0/saved_models.p")
-    models = pickle.load(open(model_path, 'rb'))
+    models = pickle.load(open(model_path, "rb"))
     compute_S_block = True
     ref_direct = os.path.join(auorg_dir, "auorg-1-1")
     rep_mode = "old"
     dest = os.path.join(test_data_dir, "refacted_joined_spline_run_deriv_delete")
     spl_ngrid = 50
-    
-    #Write the new SKFs using the new framework
-    write_skfs(models, atom_nums, atom_masses, compute_S_block, ref_direct, rep_mode, dest, spl_ngrid)
-    
-    #Now run them throughn DFTB+ with the proper settings from run_organics
-    skf_dir = os.path.join(os.getcwd(), "test_files", "refacted_joined_spline_run_deriv_delete")
-    
-    err_Ha, err_kcal, _ = run_organics(ani1_path, max_config, maxheavy, allowed_Zs, target, skf_dir,
-                                    exec_path, pardict, rep_setting)
-    
+
+    # Write the new SKFs using the new framework
+    write_skfs(
+        models, atom_nums, atom_masses, compute_S_block, ref_direct, rep_mode, dest, spl_ngrid
+    )
+
+    # Now run them throughn DFTB+ with the proper settings from run_organics
+    skf_dir = os.path.join(test_data_dir, "refacted_joined_spline_run_deriv_delete")
+
+    err_Ha, err_kcal, _ = run_organics(
+        ani1_path,
+        max_config,
+        maxheavy,
+        allowed_Zs,
+        target,
+        skf_dir,
+        exec_path,
+        pardict,
+        rep_setting,
+    )
+
     print(f"Error in Hartrees: {err_Ha}")
     print(f"Error in kcal/mol: {err_kcal}")
-    
-    assert(abs(err_kcal - refacted_benchmark) < tol)
-    
+
+    assert abs(err_kcal - refacted_benchmark) < tol
+
     if clear_direc:
         print("Doing some cleanup...")
         shutil.rmtree(os.path.join(test_data_dir, "skf_8020_100knot_deriv_delete"))
         shutil.rmtree(os.path.join(test_data_dir, "refacted_joined_spline_run_deriv_delete"))
-    
+
     print("New SKF framework tests passed")
+
 
 def run_skf_tests(clear_direc: bool = True):
     test_determine_index()
@@ -128,6 +149,5 @@ def run_skf_tests(clear_direc: bool = True):
 
 
 #%% Main block
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_skf_tests(True)
-
